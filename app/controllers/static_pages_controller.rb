ef8cf581150ff457a53b7a739efc5f77d046228f
Richard Ten <rten04@icloud.com>
2023-06-27 10:45:49 -0400

    Finish filling in #index action

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,15 @@
 class StaticPagesController < ApplicationController
 
     def index
-        
+        flickr = Flickr.new(Figaro.env.flickr_api_key, Figaro.env.flickr_shared_secret)
+
+        if params[:user_id]
+            begin
+                @photos = flickr.people.getPublicPhotos(user_id: params[:user_id])
+                flash[:error] = nil
+            rescue
+                flash[:error] = 'UserID cannot be found'
+            end
+        end
     end
 end
